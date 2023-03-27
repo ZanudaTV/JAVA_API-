9dee161d1f201e44e96c47ecce87eb59840074e8
ZanudaTV <118472943+ZanudaTV@users.noreply.github.com>
2023-03-27 22:10:47 +0300

    Update Task1.java
    123

--- Less2/Task1.java
+++ Less2/Task1.java
@@ -4,7 +4,7 @@ package Less2;
 
 public class Task1 {
     // Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса,
-    // используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки.
+    // используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки..
     // Если значение null, то параметр не должен попадать в запрос.
     // Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
     public static void main(String[] args) {
